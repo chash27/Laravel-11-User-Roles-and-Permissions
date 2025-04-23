7f80d9869620377fca1a63235c7cb2904af82d8f
Ashish Chaudhari <chash27@gmail.com>
2025-04-23 20:41:08 +0530

    hello again

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-  
+
 class CreateAdminUserSeeder extends Seeder
 {
     /**
@@ -16,17 +16,17 @@ class CreateAdminUserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'Hardik Savani', 
+            'name' => 'Ashish Chaudhari',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456')
         ]);
-        
+
         $role = Role::create(['name' => 'Admin']);
-         
+
         $permissions = Permission::pluck('id','id')->all();
-       
+
         $role->syncPermissions($permissions);
-         
+
         $user->assignRole([$role->id]);
     }
 }
