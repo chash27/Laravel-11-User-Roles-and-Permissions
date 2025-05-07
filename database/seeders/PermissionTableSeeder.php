b12613083d90a40f2e15a06b9673e090093f98e7
DivyaJyot <chash27@gmail.com>
2025-05-07 11:27:51 +0530

    Update PermissionTableSeeder.php
    Initial Commit

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-  
+
 class PermissionTableSeeder extends Seeder
 {
     /**
@@ -14,6 +14,10 @@ class PermissionTableSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
+           'user-list',
+           'user-create',
+           'user-edit',
+           'user-delete',
            'role-list',
            'role-create',
            'role-edit',
@@ -23,7 +27,7 @@ class PermissionTableSeeder extends Seeder
            'product-edit',
            'product-delete'
         ];
-        
+
         foreach ($permissions as $permission) {
              Permission::create(['name' => $permission]);
         }
