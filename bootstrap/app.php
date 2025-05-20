5faca685d4375de39f182d419d881faf6822ffee
DivyaJyot <chash27@gmail.com>
2025-05-20 12:10:01 +0530

    Add minify package for code security

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class
         ]);
+        $middleware->web(append: [
+            \Fahlisaputra\Minify\Middleware\MinifyHtml::class,
+            \Fahlisaputra\Minify\Middleware\MinifyCss::class,
+            \Fahlisaputra\Minify\Middleware\MinifyJavascript::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
